c098044aabe8e4e12c7869c47a2d4ffbcf329c5f
Ayush rai <raiayush2209-web>
2026-09-14 12:19:30 +0530

    "fix cors policy "

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,11 @@ if (!config.PINECONE_API_KEY) {
 }
 
 // Middleware
-app.use(cors());
+
+
+app.use(cors({
+  origin: 'https://ai-knowledge-assistant-frontend-beta.vercel.app'
+}));
 app.use(express.json({ limit: '15mb' }));
 app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 
